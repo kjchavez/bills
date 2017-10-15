ecb86b4a5315d3b0b9fefe7c9bdc31f1bc640aa1
Kevin Chavez <kevin.j.chavez@gmail.com>
2017-10-15 12:04:02 -0700

    calling .cuda() on the model. Added mroe comments for future self.

--- bills/models/train.py
+++ bills/models/train.py
@@ -48,7 +48,7 @@ class BillDataSet(Dataset):
         with open(join(billdir, BillDataSet.LABEL)) as fp:
             label = json.load(fp)
             status = label['status']
-            class_idx = _status_to_class_idx(label['status'])
+            class_idx = torch.LongTensor([_status_to_class_idx(label['status'])])
 
         sample = {"document": document,
                   "status": status,
@@ -58,12 +58,6 @@ class BillDataSet(Dataset):
 
         return sample
 
-def load_vocab(N=10000):
-    filename = os.path.join(PROCESSED_DATA, "vocab.txt")
-    with open(filename) as fp:
-        tokens = [line.strip() for line in fp]
-
-    return tokens[0:N]
 
 class InputLayer(object):
     def __init__(self, vocab_file, vocab_size):
@@ -97,29 +91,44 @@ def _matrix_dims(size):
         N *= dim
     return (N, size[-1])
 
+# Since torch matrix multiplication operations only work with 2D tensors, this little helper makes
+# it convenient to work with higher dim tensors.
+def _tensor_dot(t1, t2):
+    """ Multiples an (N, *, K) tensor by a (K, M) matrix. Returns (N, *, M) tensor. """
+    original_size = t1.size()
+    x = t1.resize(*_matrix_dims(t1.size()))
+    return torch.mm(x, t2).resize(*original_size[0:-1])
+
 class AttentionReduction(torch.nn.Module):
+    """ Reduces a matrix of embeddings to a single vector using attention.
+
+    Takes an M x N x D matrix and reduces along the 2nd dimension using a weighted sum based on
+    attention computed using a learned context vector.
+
+    Returns:
+        M x D matrix.
+    """
     def __init__(self, input_dim, attention_dim):
         super(AttentionReduction, self).__init__()
         self.linear = torch.nn.Linear(input_dim, attention_dim)
         self.context = torch.nn.Parameter(torch.randn(attention_dim, 1))
 
     def forward(self, matrix):
-        """
-        Args:
-            matrix:  (N, M, input_dim)
-        """
-        x = F.tanh(self.linear(matrix))
-        logging.debug("After tanh: %s", x.size())
-        y = torch.mm(x.resize(*_matrix_dims(x.size())), self.context).resize(*x.size()[0:-1])
-        logging.debug("After MM with context: %s", y.size())
+        logging.debug("Input matrix size: %s", matrix.size())
+        hidden = F.tanh(self.linear(matrix))
+        y = _tensor_dot(hidden, self.context)
+        logging.debug("After MM with context: %s", hidden.size())
         att = F.softmax(y)
-        logging.debug("Att. shape: %s", att.size())
+        logging.debug("Attention weights size: %s", att.size())
+
+        # To make the broadcast operation work, we need the number of dimensions to match.
+        # matrix is M x N x D. att is M x N -- so we make it M x N x 1.
         att = torch.unsqueeze(att, -1)
-        # element-wise, broadcast multiplication
         weighted_matrix = matrix * att
-        reduced_vec = torch.sum(weighted_matrix, 1)
-        logging.debug("Reduced vec shape: %s", reduced_vec.size())
-        return reduced_vec
+
+        summary = torch.sum(weighted_matrix, 1)
+        logging.debug("Summary vector size: %s", summary.size())
+        return summary
 
 
 class HierarchicalAttention(torch.nn.Module):
@@ -127,26 +136,33 @@ class HierarchicalAttention(torch.nn.Module):
                  sentence_attention_dim=32):
         super(HierarchicalAttention, self).__init__()
         # Embedding matrix for tokens.
+        # NOTE(kjchavez): We probably want to keep this on the CPU, even if we move the rest of the
+        # model to a GPU.
+        # TODO(kjchavez): Clean way to do this?
         self.embedding = torch.nn.Embedding(vocab_size, embedding_dim)
 
         # Parameters for creating sentence embeddings
         self.word_lstm = torch.nn.LSTM(input_size=embedding_dim, hidden_size=hidden_size, num_layers=1,
-                                       bidirectional=True, batch_first=True).cuda()
+                                       bidirectional=True, batch_first=True)
 
         # Note, the embedding is going to be 2*hidden_size because we are using a bi-directional
         # LSTM.
-        self.word_attention = AttentionReduction(2*hidden_size, word_attention_dim).cuda()
+        self.word_attention = AttentionReduction(2*hidden_size, word_attention_dim)
 
         # And for creating full document embeddings.
         self.sent_lstm = torch.nn.LSTM(input_size=2*hidden_size, hidden_size=hidden_size, num_layers=1,
-                                       bidirectional=True, batch_first=True).cuda()
-        self.sentence_attention = AttentionReduction(2*hidden_size, sentence_attention_dim).cuda()
+                                       bidirectional=True, batch_first=True)
+        self.sentence_attention = AttentionReduction(2*hidden_size, sentence_attention_dim)
 
     def forward(self, x):
         logging.debug("Input shape: %s", x.size())
         flat_doc = x.resize(*_matrix_dims(x.size()))
         x_embed = self.embedding(flat_doc)
-        x_embed = x_embed.cuda()
+
+        # TODO(kjchavez): If we move the embedding matrix to the CPU, then we'll have to call,
+        #
+        #   x_embed = x_embed.cuda()
+
         # NOTE: We shouldn't run the lstm over the whole sequence, since lengths vary.
         # Use packed/padded utils.
         hidden, out = self.word_lstm(x_embed)
@@ -169,7 +185,7 @@ class DocumentClassifier(torch.nn.Module):
     def __init__(self, word_embed_dim=50, hidden_size=8, vocab_size=100, num_classes=2):
         super(DocumentClassifier, self).__init__()
         self.hier_att = HierarchicalAttention(word_embed_dim, hidden_size, vocab_size)
-        self.linear = torch.nn.Linear(hidden_size*2, num_classes).cuda()
+        self.linear = torch.nn.Linear(hidden_size*2, num_classes)
 
     def forward(self, x):
         logits = self.linear(self.hier_att(x))
@@ -179,25 +195,38 @@ class DocumentClassifier(torch.nn.Module):
 
 def smoke_test():
     logging.basicConfig(level=logging.INFO)
+
+    # Creates a dataset from the processed data that was created by bills/data/make_dataset.py.
+    # Trying out the project structure from Cookie Cutter Data Science.
     dset = BillDataSet(consts.PROCESSED_DATA_DIR, os.path.join(consts.PROCESSED_DATA_DIR, "train.txt"))
     dataloader = DataLoader(dset, batch_size=4, shuffle=True, num_workers=4)
 
-    VOCAB_SIZE = 100
-    WORD_EMBED_DIM = 50
-    HIDDEN_SIZE = 8
+    # Hyper parameters for a sanity check.
+    # TODO(kjchavez): What's the "right" way to work with hyperparameters in PyTorch?
+    VOCAB_SIZE = 5000
+    WORD_EMBED_DIM = 100
+    HIDDEN_SIZE = 50
+
     inputs = InputLayer(os.path.join(consts.PROCESSED_DATA_DIR, "vocab.txt"), VOCAB_SIZE)
     model = DocumentClassifier(word_embed_dim=WORD_EMBED_DIM,
                               hidden_size=HIDDEN_SIZE,
                               vocab_size=VOCAB_SIZE)
 
+    # In PyTorch, to move an entire nn.Module to the GPU, we call .cuda(). This moves all the
+    # torch.nn.Parameters defined in the module (and submodules) to a GPU.
+    # NOTE: From docs, this should be done prior to creating the optimizer for this model.
+    model = model.cuda()
+
     optimizer = optim.SGD(model.parameters(), lr=1e-3, momentum=0.9)
     for batch in dataloader:
-        x = Variable(inputs.to_long_tensor(batch['document']))
-        logging.debug(x.size())
+        # We are intentionally not moving this to the GPU. The embedding matrix will live on the
+        # CPU. Even though we move the 'model' to gpu.
+        x = Variable(inputs.to_long_tensor(batch['document'])).cuda()
+        logging.debug("Dimensions of input: %s", x.size())
         logits = model(x)
         probs = F.softmax(logits)
-        label = Variable(batch['label'], requires_grad=False).cuda()
-        loss = F.cross_entropy(logits, label)
+        label = Variable(batch['label'].squeeze(), requires_grad=False)
+        loss = F.cross_entropy(logits, label.cuda())
         logging.info("Loss: %s", loss)
         optimizer.zero_grad()
         loss.backward()
